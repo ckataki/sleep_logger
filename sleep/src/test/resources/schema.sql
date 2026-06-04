5a10eaeec5d674432db0cee67d073f303d9e1259
CJ Kataki <1197127+ckataki@users.noreply.github.com>
2026-06-04 18:18:23 -0400

    Test schema for H2 database

---
+++ sleep/src/test/resources/schema.sql
@@ -0,0 +1,18 @@
+CREATE TABLE IF NOT EXISTS users
+(
+    id UUID DEFAULT RANDOM_UUID() PRIMARY KEY,
+    username VARCHAR NOT NULL UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS sleep_log
+(
+    id UUID DEFAULT RANDOM_UUID() PRIMARY KEY,
+    user_id VARCHAR REFERENCES users (id) ON DELETE CASCADE,
+    sleep_date DATE NOT NULL,
+    start_time TIME NOT NULL,
+    end_time TIME NOT NULL,
+    duration INT NOT NULL,
+    quality VARCHAR NOT NULL
+);
+
+CREATE UNIQUE INDEX IF NOT EXISTS user_id_sleep_date_unique_idx ON sleep_log (user_id, sleep_date);
