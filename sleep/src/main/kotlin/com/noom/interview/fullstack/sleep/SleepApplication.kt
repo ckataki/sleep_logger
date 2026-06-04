464d092c9fc9d077cc9436406800d597e6a46886
CJ Kataki <1197127+ckataki@users.noreply.github.com>
2026-06-04 18:13:14 -0400

    Detekt formatting changes to pre-existing files

--- sleep/src/main/kotlin/com/noom/interview/fullstack/sleep/SleepApplication.kt
+++ sleep/src/main/kotlin/com/noom/interview/fullstack/sleep/SleepApplication.kt
@@ -3,13 +3,15 @@ package com.noom.interview.fullstack.sleep
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
+@Suppress("UtilityClassWithPublicConstructor")
 @SpringBootApplication
 class SleepApplication {
-	companion object {
-		const val UNIT_TEST_PROFILE = "unittest"
-	}
+    companion object {
+        const val UNIT_TEST_PROFILE = "unittest"
+    }
 }
 
+@Suppress("SpreadOperator")
 fun main(args: Array<String>) {
-	runApplication<SleepApplication>(*args)
+    runApplication<SleepApplication>(*args)
 }
